387b5ea8f7e54a99599d7af38d2e32748d53dfa2
Jorge Martinelli <jorgemartinelli@Jorges-MacBook-Air.local>
2026-02-09 18:59:43 -0500

    Add test route

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, background: '#000', color: '#fff' }}>
+      <h1>TEST OK</h1>
+      <p>Routing works. Vercel is reading the App Router.</p>
+    </div>
+  );
+}
