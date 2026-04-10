486a0cd3ad620fed082380c877583a40f2f826aa
jmartinelli1312 <jmartinellir@gmail.com>
2026-04-10 02:42:55 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,37 @@ const nextConfig: NextConfig = {
     ];
   },
 
+  async headers() {
+    return [
+      {
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=300, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=300, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
